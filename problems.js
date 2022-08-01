ed62813a86ee5e7adcf07d2ef96ff26ea1e278c5
ArmanMughdusyan <arman.mughdusyan33@gmail.com>
2022-08-01 14:55:03 -0700

    new problem

--- problems.js
+++ problems.js
@@ -1,13 +1,12 @@
-let arr = [5, 6, 1, 8, 2, 7, 10, 3, 15, 13, 12];
+let a = 15;
+let b  = 12;
+let c = 25;
 
-for(let j = 0; j < arr.length; j++) {
-    for (let i = 0; i < arr.length - 1 ; i++) {
-        if (arr[i] > arr[i+1]) {
-            let a = arr[i];
-            arr[i] = arr [i+1] ;
-            arr[i+1] = a;
-        };
-    };
-};
- 
-console.log(arr);
+if (a < b) {
+    a = b;
+}
+
+if (a < c){
+    a = c
+} ;
+alert(a);
