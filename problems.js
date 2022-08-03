4f050e99b1b5f22fed3717f4e36b69e1aa988120
ArmanMughdusyan <arman.mughdusyan33@gmail.com>
2022-08-03 06:24:11 -0700

    "simple and complex numbers

--- problems.js
+++ problems.js
@@ -1,13 +1,13 @@
-let arr = [2, 6, 3, 7, 15, 17] ;
+let arr = [2, 6, 3, 7, 0, 15, 17] ;
 let arr1 =[];
 let arr2 = [];
 let j = 0;
 let k = 0;
 for (let i = 0; i < arr.length; i++) {
-    if ( (arr[i] % 2 == 0 && arr[i] != 2) || (arr[i] % 3 == 0 && arr[i] != 3)) {
+    if ( ((arr[i] % 2 == 0 && arr[i] != 2) || (arr[i] % 3 == 0 && arr[i] != 3)) && arr[i] != 0) {
         arr1[j] = arr[i];
         j++;
-    } else {
+    } else if (arr[i] != 0){
         arr2[k] = arr[i];
         k++;
     }
