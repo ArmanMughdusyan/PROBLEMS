33a0419a0f1837fd1346261bb9eb6abd520e9a80
ArmanMughdusyan <arman.mughdusyan33@gmail.com>
2022-08-03 08:23:06 -0700

    string

--- problems.js
+++ problems.js
@@ -1,16 +1,9 @@
-let arr = [2, 6, 3, 7, 0, 15, 17] ;
-let arr1 =[];
-let arr2 = [];
-let j = 0;
-let k = 0;
-for (let i = 0; i < arr.length; i++) {
-    if ( ((arr[i] % 2 == 0 && arr[i] != 2) || (arr[i] % 3 == 0 && arr[i] != 3)) && arr[i] != 0) {
-        arr1[j] = arr[i];
-        j++;
-    } else if (arr[i] != 0){
-        arr2[k] = arr[i];
-        k++;
+function k (a) {
+    if (typeof(a) == "string") {
+        console.log(true);;
+    } else {
+        console.log(false);
     }
-};
-console.log(arr1);
-console.log(arr2);
+}
+let b = "Arman";
+k(b);
